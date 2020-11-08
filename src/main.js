1d1c999e8373ae48e32f08c8a1401859e0687bf3
mainlyetcetera <ericcampbell294@gmail.com>
2020-11-08 12:21:13 -0700

    Have DOM start a game on page load

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
+var game;
 var board = document.querySelector('.game-board');
 
+window.onload = setGame;
 board.addEventListener('click', stuff);
+
+function startGame() {
+  var newGame = new Game();
+  return newGame;
+}
+
+function setGame() {
+  game = startGame();
+}
 
 function stuff(event) {
   displayID(event.target.id);
